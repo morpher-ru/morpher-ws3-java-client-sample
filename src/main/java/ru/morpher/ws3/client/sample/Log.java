65739be2ee1178ef702bcf021817c133323c3d60
andrii-r <atomix@kievnet.com.ua>
2017-12-10 16:35:50 +0200

    Create separate usage example project that use morpher-ws3-java-client Maven dependency

---
+++ src/main/java/ru/morpher/ws3/client/sample/Log.java
@@ -0,0 +1,15 @@
+package ru.morpher.ws3.client.sample;
+
+import static java.lang.String.format;
+
+class Log {
+    protected static final String PREMIUM = "*****";
+
+    protected static void log(String message) {
+        System.out.println(message);
+    }
+
+    protected static void log(String messageWithPlaceholders, Object... params) {
+        System.out.println(format(messageWithPlaceholders, params));
+    }
+}
